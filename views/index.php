9e915f7ca076c4b66bcfffffa133183281a678d3
kelvinlnx <57615312+kelvinlnx@users.noreply.github.com>
2024-06-15 20:43:43 +0800

    Create index.php

---
+++ views/index.php
@@ -0,0 +1,6 @@
+<h2>Welcome to My Website</h2>
+<p>Select an option:</p>
+<ul>
+    <li><a href="query.php">Query Data</a></li>
+    <li><a href="create.php">Create Table and Insert Data</a></li>
+</ul>
