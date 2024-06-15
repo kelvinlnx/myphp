2e0e1f00c29fbfd678caf9abc8aa361330370e22
kelvinlnx <57615312+kelvinlnx@users.noreply.github.com>
2024-06-15 21:51:05 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,10 @@
 <?php
 return [
-    'db_host' => 'mydb.dbproj',
-    'db_user' => 'kelvin',
-    'db_pass' => 'abc',
-    'db_name' => 'training'
+    'db_host' => getent('DB_HOST'),
+    'db_user' => getent('DB_USER'),
+    'db_pass' => getent('DB_PASS'),
+    'db_name' => getent('DB_NAME'),
+    'env_msg' => getent('MSG'),
+    'env_value1' => getent('VALUE1')
 ];
 ?>
