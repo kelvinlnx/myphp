955a1c472974e300f91140db3278803f349d154b
kelvinlnx <57615312+kelvinlnx@users.noreply.github.com>
2024-06-15 22:17:46 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'db_host' => getent('DB_HOST'),
+    'db_host' => htmlspecialchars(getent('DB_HOST'), ENT_QUOTES, 'UTF-8');
     'db_user' => getent('DB_USER'),
     'db_pass' => getent('DB_PASS'),
     'db_name' => getent('DB_NAME'),
