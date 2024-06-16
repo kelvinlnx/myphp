60bc9fa05c97b783b6f077772009a49a5e63d4df
kelvinlnx <57615312+kelvinlnx@users.noreply.github.com>
2024-06-16 21:15:34 +0800

    Update query.php

--- views/query.php
+++ views/query.php
@@ -10,10 +10,6 @@ function safe_output($value) {
 $config = include(__DIR__ . '/../config/config.php');
 $servername = $config['db_host'];
 
-// Database operations
-// $server_ip = gethostbyname($servername);
-$server_ip = gethostname($servername);
-
 // Display server name and IP address
 echo '<h2>Server Information</h2>';
 echo '<table>';
@@ -21,36 +17,43 @@ echo '<tr><th>Server Name</th><td>' . safe_output($_SERVER['SERVER_NAME']) . '</
 echo '<tr><th>Server IP Address</th><td>' . safe_output($_SERVER['SERVER_ADDR']) . '</td></tr>';
 echo '</table>';
 
+
+// Display environment variables if set
+echo '<h2>Environment Variables</h2>';
+if ($config['env_msg'] !== false || $config['env_value1'] !== false) {
+    echo '<table>';
+    echo '<tr><th>Environment Variable</th><th>Value</th></tr>';
+    echo '<tr><td>' . safe_output('MSG') . '</td>';
+    if ($config['env_msg'] !== false ) {
+        echo '<td>' . safe_output($config['env_msg']) . '</td>';
+    } else {
+        echo '<td class="warning">Warning: ' . safe_output('MSG') . ' is not set!</td>';
+    }
+    echo '</tr>';
+    
+    echo '<tr><td>' . safe_output('VALUE1') . '</td>';
+    if ($config['env_value1'] !== false ) {
+        echo '<td>' . safe_output($config['env_value1']) . '</td>';
+    } else {
+        echo '<td class="warning">Warning: ' . safe_output('VALUE1') . ' is not set!</td>';
+    }    
+    echo '</tr>';
+    
+    echo '</table>';
+} else {
+    echo '<p class="warning">Environment variables MSG and VALUE1 are not set. Unable to display environment variables.</p>';
+}
+
+// Database operations
+// $server_ip = gethostbyname($servername);
+$server_ip = gethostname($servername);
+
+echo '<h2>Database Query</h2>';
 if ($server_ip == $servername) {
     echo '<p class="warning">The database server name "' . safe_output($servername) . '" could not be resolved. Unable to connect to the database.</p>';
+    echo '<p class="warning">Did you set the environment variables (DB_HOST, DB_NAME, DB_USER and DB_PASS)?</p>';
 } else {
     $conn = getDbConnection();
-
-    // Display environment variables if set
-    if ($config['env_msg'] !== false || $config['env_value1'] !== false) {
-        echo '<h2>Environment Variables</h2>';
-        echo '<table>';
-        echo '<tr><th>Environment Variable</th><th>Value</th></tr>';
-        echo '<tr><td>' . safe_output('MSG') . '</td>';
-        if ($config['env_msg'] !== false ) {
-            echo '<td>' . safe_output($config['env_msg']) . '</td>';
-        } else {
-            echo '<td class="warning">Warning: ' . safe_output('MSG') . ' is not set!</td>';
-        }
-        echo '</tr>';
-        
-        echo '<tr><td>' . safe_output('VALUE1') . '</td>';
-        if ($config['env_value1'] !== false ) {
-            echo '<td>' . safe_output($config['env_value1']) . '</td>';
-        } else {
-            echo '<td class="warning">Warning: ' . safe_output('VALUE1') . ' is not set!</td>';
-        }    
-        echo '</tr>';
-        
-        echo '</table>';
-    } else {
-        echo '<p class="warning">Environment variables MSG and VALUE1 are not set. Unable to display environment variables.</p>';
-    }
 
     // Check if the 'users' table exists
     $table_exists = $conn->query("SHOW TABLES LIKE 'users'")->num_rows > 0;
